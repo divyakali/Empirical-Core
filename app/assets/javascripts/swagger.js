bd1b77c958160f6df2f8cf3bf95ae478a1ea69b0
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-08-10 12:04:50 -0700

    Forgot swagger.js

---
+++ app/assets/javascripts/swagger.js
@@ -0,0 +1,12 @@
+//= require swagger-ui/lib/jquery-1.8.0.min.js
+//= require swagger-ui/lib/jquery.slideto.min.js
+//= require swagger-ui/lib/jquery.wiggle.min.js
+//= require swagger-ui/lib/jquery.ba-bbq.min.js
+//= require swagger-ui/lib/handlebars-2.0.0.js
+//= require swagger-ui/lib/underscore-min.js
+//= require swagger-ui/lib/backbone-min.js
+//= require swagger-ui/lib/swagger-client.js
+//= require swagger-ui.js
+//= require swagger-ui/lib/highlight.7.3.pack.js
+//= require swagger-ui/lib/marked.js
+//= require swagger-ui/lib/swagger-oauth.js
